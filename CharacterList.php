2e6cb0547930edb4b14e8f16e01101cc8d2b4832
RunaLea <runalea@outlook.com>
2025-02-28 12:29:37 +0100

    first commit

---
+++ CharacterList.php
@@ -0,0 +1,27 @@
+<?php
+// CORS FILTER
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+
+    // 1. Databaseverbinding maken
+$servername = "forumpjedb.mysql.database.azure.com";
+$username = "felixadmin";
+$password = "uiop7890UIOP&*()";
+$dbname = "character_gen_runa";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// 2. SQL-query uitvoeren
+$sql = "SELECT * FROM characters";
+$result = $conn->query($sql);
+$records = []; // Array om resultaten op te slaan
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $records[] = $row; // Voeg elke rij toe aan de array
+    }
+}
+
+echo json_encode($records, JSON_PRETTY_PRINT); // Omgezet naar JSON
+?>
---
+++ CharacterListInsert.php
@@ -0,0 +1,38 @@
+<?php
+$data = json_decode(file_get_contents("php://input"), true);
+print_r($data);
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: Content-Type");
+  // 1. We halen gegevens op uit de URL-parameters
+    $char_name = $data["char_name"];
+    $char_lvl = $data["char_lvl"];
+
+  // 2. Databaseverbinding
+  $servername = "forumpjedb.mysql.database.azure.com";
+  $username = "felixadmin";
+  $password = "uiop7890UIOP&*()";
+  $dbname = "character_gen_runa";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check of de verbinding lukt
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  // 3. SQL-query voor toevoegen van data
+  // Let op de kolomnamen in je table
+  $sql = "INSERT INTO characters (char_name, char_lvl)
+          VALUES ('$char_name', '$char_lvl')";
+echo $sql;
+  // 4. Voer de query uit en check of het gelukt is
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  // 5. Sluit de verbinding
+  $conn->close();
+?>
