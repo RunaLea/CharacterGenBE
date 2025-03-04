9488d3c5ede8143163b8a798bf54854df261a9cd
RunaLea <runalea@outlook.com>
2025-03-04 16:30:49 +0100

    added races to the sql statement in character list php

--- CharacterList.php
+++ CharacterList.php
@@ -13,7 +13,9 @@ $dbname = "character_gen_runa";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // 2. SQL-query uitvoeren
-$sql = "SELECT * FROM characters";
+$sql = "SELECT characters.id, characters.char_name, characters.char_lvl, races.race_title, characters.race_id
+FROM characters
+INNER JOIN races ON races.id=characters.race_id";
 $result = $conn->query($sql);
 $records = []; // Array om resultaten op te slaan
 
