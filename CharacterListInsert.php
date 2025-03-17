feeb3cc20d2d05c249726d5f5954a233249d5c10
“RunaLeagit config --[200~global user.email “[valid-email]”git config --global user.name “RunaLea <runalea@outlook.com>
2025-03-17 16:58:27 +0100

    updated the insert to add more data

--- CharacterListInsert.php
+++ CharacterListInsert.php
@@ -6,7 +6,15 @@ print_r($data);
     header("Access-Control-Allow-Headers: Content-Type");
   // 1. We halen gegevens op uit de URL-parameters
     $char_name = $data["char_name"];
+    $race_id = $data["race_id"];
+    $bg_id = $data["bg_id"];
     $char_lvl = $data["char_lvl"];
+    $char_str = $data["char_str"];
+    $char_dex = $data["char_dex"];
+    $char_con = $data["char_con"];
+    $char_int = $data["char_int"];
+    $char_wis = $data["char_wis"];
+    $char_cha = $data["char_cha"];
 
   // 2. Databaseverbinding
 
@@ -29,8 +37,8 @@ print_r($data);
 
   // 3. SQL-query voor toevoegen van data
   // Let op de kolomnamen in je table
-  $sql = "INSERT INTO characters (char_name, char_lvl, race_id)
-          VALUES ('$char_name', '$char_lvl', $race_id)";
+  $sql = "INSERT INTO characters (char_name, race_id, bg_id, char_lvl, char_str, char_dex, char_con, char_int, char_wis, char_cha)
+          VALUES ('$char_name', '$race_id', '$bg_id', '$char_lvl', '$char_str', '$char_dex', '$char_con', '$char_int', '$char_wis', '$char_cha')";
 echo $sql;
   // 4. Voer de query uit en check of het gelukt is
   if ($conn->query($sql) === TRUE) {
